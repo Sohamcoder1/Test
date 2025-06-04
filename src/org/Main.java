5121cc8a9b56ecd19c73226cd8fbf48aafb836b6
Sohamcoder1 <sohmukhopadhyay@gmail.com>
2025-06-04 12:27:39 +0530

    Initialize first time

---
+++ src/org/Main.java
@@ -0,0 +1,14 @@
+package org;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		University.Department show = new University.Department("Computer Science", 28);
+		show.displayInfo();
+		
+		
+
+	}
+
+}
---
+++ src/org/University.java
@@ -0,0 +1,31 @@
+package org;
+
+public class University {
+	
+	static class Department{
+		
+		private String departmentName;
+		private int facultyCount;
+		
+		public Department( String departmentName, int facultyCount ) {
+		this.departmentName = departmentName;
+		this.facultyCount = facultyCount;
+		}
+		
+		
+		public void displayInfo() {
+			
+			System.out.println("departmentName :" + departmentName);
+			System.out.println("facultyCount :" + facultyCount);
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+	}
+
+}
